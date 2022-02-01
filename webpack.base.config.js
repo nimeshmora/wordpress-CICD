92fb6d004cc90596ae4169e2834dddf4321f9a52
Nimesha Jinarajadasa <mhnimesh@gmail.com>
2022-02-01 22:26:57 +0530

    Create webpack.base.config.js

---
+++ webpack.base.config.js
@@ -0,0 +1,22 @@
+'use strict';
+module.exports = {
+  entry: {
+    main: './site/theme/js/main.js'
+  },
+  output: {
+    filename: 'bundle.js',
+    path: './site/theme/assets/js'
+  },
+  plugins: [],
+  eslint: {
+    configFile: './.eslintrc'
+  },
+  module: {
+    preLoaders: [{
+      test: /\.js$/,
+      loader: 'eslint-loader',
+      exclude: /node_modules/
+    }]
+  },
+  watch: false
+};
